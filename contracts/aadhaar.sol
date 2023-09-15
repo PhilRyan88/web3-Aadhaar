8ea8266c6c6b7fee79db6d889955a1d84d5e3346
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-09-15 23:44:21 +0530

    Update aadhaar.sol

--- contracts/aadhaar.sol
+++ contracts/aadhaar.sol
@@ -11,9 +11,16 @@ contract Aadhar{
 
     }
     mapping(uint256=>Detail) public Details;
-    function Enter(uint256 _id, string memory _name, string memory _aadr )public view{
+    function Enter( uint256 _id, string memory _name, uint256 _age, string memory _gender, string memory _state,string memory _district)public {
+        Details[_id] = Detail(_name,_age,_gender,_state,_district);
 
 
+
+
+    }
+    function Get(uint256 _id, string memory _name, uint256 _age, string memory _gender, string memory _state,string memory _district)public view returns {
+        Detail storage detail = Details[_id];
+        return (detail.name, detail.age, detail.gender, detail.state, detail.district);
 
     }
 
