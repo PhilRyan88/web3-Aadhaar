9d14e928cad70cc2b9b3724e3848022a2a9fa4fc
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-09-15 23:31:48 +0530

    Update aadhaar.sol

--- contracts/aadhaar.sol
+++ contracts/aadhaar.sol
@@ -1,13 +1,19 @@
 pragma solidity ^0.8.19;  
 contract Aadhar{
     struct Detail{
-        uint256 id;
+       
         string name;
-        string addr;
+        uint256 age;
+        string gender;
+
+        string state;
+        string district;
 
     }
     mapping(uint256=>Detail) public Details;
     function Enter(uint256 _id, string memory _name, string memory _aadr )public view{
+
+
 
     }
 
