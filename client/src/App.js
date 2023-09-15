3e4b970477433314e61e941be7f51c2fe0fe926b
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-09-16 01:09:15 +0530

    front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>Web3 Aadhaar Minter</h1>
+      </center>
+      <br />
+      <br />
+      <center>
+        <button>Connect Wallet</button>
+      </center>
     </div>
   );
-}
+};
 
 export default App;
