73b58102eafc82a96d9092dd5f5b1c79506e4e75
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-09-16 00:41:26 +0530

    Update aadhaar.sol

--- contracts/aadhaar.sol
+++ contracts/aadhaar.sol
@@ -10,16 +10,13 @@ contract Aadhar{
         string district;
 
     }
-    mapping(uint256=>Detail) public Details;
-    function Enter( uint256 _id, string memory _name, uint256 _age, string memory _gender, string memory _state,string memory _district)public {
-        Details[_id] = Detail(_name,_age,_gender,_state,_district);
-
-
-
-
+    mapping(address=>Detail) public Details;
+   function enter(string memory _name, uint256 _age, string memory _gender, string memory _state, string memory _district) public {
+        address _id = msg.sender;
+        Details[_id] = Detail(_name, _age, _gender, _state, _district);
     }
-    function Get(uint256 _id, string memory _name, uint256 _age, string memory _gender, string memory _state,string memory _district)public view returns {
-        Detail storage detail = Details[_id];
+    function Get(address _id) public view returns (string memory, uint256, string memory, string memory, string memory) {
+        Detail storage detail = Details[_id]; //used storage type, mapped the Detail using storage type and a variable is declared 
         return (detail.name, detail.age, detail.gender, detail.state, detail.district);
 
     }
