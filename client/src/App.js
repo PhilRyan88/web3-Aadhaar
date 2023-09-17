b56779549d365b6788dcd9212097af1f070f536e
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-09-17 17:48:34 +0530

    wallet

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,41 @@
 import React from "react";
-
+import { useEffect, useState } from "react";
+//import {useNavigate} from 'react-router-dom';
 const App = () => {
+  const [isWalletInstalled, setIsWalletInstalled] = useState(false); //for checking if metamask installed or not
+  const [account, setAccount] = useState(null); // for checking connection status
+
+  useEffect(() => {
+    if (window.ethereum) {
+      //checks if metamask extension is there
+      setIsWalletInstalled(true); // if it's there then changes the initial value of false to true
+    }
+  }, []);
+
+  async function connect() {
+    window.ethereum
+      .reques({
+        method: "eth_requestAccounts",
+      })
+      .then((accounts) => {
+        setAccount(accounts[0]);
+      })
+      .catch((error) => {
+        alert("Something went wrong");
+      });
+  }
+
+  if (account === null) {
+    return (
+      <div>
+        {isWalletInstalled ? (
+          <button onClick={connect}>Connect</button>
+        ) : (
+          <p>Install metamask</p>
+        )}
+      </div>
+    );
+  }
   return (
     <div>
       <center>
